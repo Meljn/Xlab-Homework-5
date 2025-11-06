34d1ca09bb4b03266654ce9fbfb6c0ffc13ec0eb
Meljn <ilya.mel2018@mail.ru>
2025-11-06 22:53:35 +0300

    first commit

---
+++ Homework 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public interface IDamageable<in T>
+{
+    void TakeDamage(T monster);
+}
+
+public class FireDamage : IDamageable<Monster>
+{
+    public void TakeDamage(Monster monster)
+    {
+        Console.WriteLine($"{monster.GetType().Name} получил урон!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        IDamageable<Monster> fire = new FireDamage();
+        
+        TakeDamageToMonsters(fire, new Zombie());
+        TakeDamageToMonsters(fire, new Skillet());
+    }
+    public static void TakeDamageToMonsters(IDamageable<Zombie> damage, Zombie zombie)
+    {
+        damage.TakeDamage(zombie);
+    }
+    public static void TakeDamageToMonsters(IDamageable<Skillet> damage, Skillet skillet)
+    {
+        damage.TakeDamage(skillet);
+    }
+}
+
+public class Monster { }
+public class Zombie : Monster { }
+public class Skillet : Monster { }
